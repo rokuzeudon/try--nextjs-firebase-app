c1e753e9bb59e32bef04549127bbb62311221c38
rokuzeudon <g-awaya@60dd.co>
2022-02-27 10:02:44 +0900

    ユーザー詳細画面を作成する

---
+++ pages/users/[uid].tsx
@@ -0,0 +1,38 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { User } from '../../models/User'
+import { collection, doc, getDoc, getFirestore } from 'firebase/firestore'
+
+type Query = {
+  uid: string
+}
+
+export default function UserShow() {
+  const [user, setUser] = useState<User>(null)
+  const router = useRouter()
+  const query = router.query as Query
+
+  useEffect(() => {
+    if (query.uid === undefined) {
+      return
+    }
+    async function loadUser() {
+      console.log(query)
+      const db = getFirestore()
+      const ref = doc(collection(db, 'users'), query.uid)
+      const userDoc = await getDoc(ref)
+
+      if (!userDoc.exists()) {
+        console.log('returned')
+        return
+      }
+
+      const gotUser = userDoc.data() as User
+      gotUser.uid = userDoc.id
+      setUser(gotUser)
+    }
+    loadUser()
+  }, [query.uid])
+
+  return <div>{user ? user.name : 'ロード中…'}</div>
+}
