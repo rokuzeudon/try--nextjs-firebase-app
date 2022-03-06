ce92ae06531f0695083b47552ef4da02f8691990
rokuzeudon <g-awaya@60dd.co>
2022-03-06 16:26:53 +0900

    並び順を変更する

--- pages/questions/received.tsx
+++ pages/questions/received.tsx
@@ -3,6 +3,7 @@ import {
   collection,
   getDocs,
   getFirestore,
+  orderBy,
   query,
   where,
 } from 'firebase/firestore'
@@ -28,7 +29,8 @@ export default function QuestionsReceived() {
       const db = getFirestore()
       const q = query(
         collection(db, 'questions'),
-        where('receiverUid', '==', user.uid)
+        where('receiverUid', '==', user.uid),
+        orderBy('createdAt', 'desc')
       )
       const snapshot = await getDocs(q)
 
@@ -46,7 +48,6 @@ export default function QuestionsReceived() {
 
     loadQuestions()
   }, [process.browser, user])
-
 
   return (
     <Layout>
