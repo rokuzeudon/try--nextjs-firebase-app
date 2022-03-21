98db0f680025eb715fb661ed4d60264f56e37aae
rokuzeudon <g-awaya@60dd.co>
2022-03-21 15:38:48 +0900

    フッター・ヘッダー

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import { ToastContainer } from 'react-toastify'
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Layout({ children }) {
   const title = 'My質問回答サービス'
@@ -17,6 +18,10 @@ export default function Layout({ children }) {
           key="ogDescription"
           content={description}
         />
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        ></link>
       </Head>
       <nav
         className="navbar navbar-expand-lg navbar-light mb-3"
@@ -24,18 +29,26 @@ export default function Layout({ children }) {
       >
         <div className="container">
           <div className="mr-auto">
-            <a className="navbar-brand" href="#">
-              Navbar
+            <a className="navbar-brand" href="/">
+              My質問サービス
             </a>
           </div>
-          <form className="d-flex">
-            <button className="btn btn-outline-primary" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </nav>
       <div className="container">{children}</div>
+      <nav className="navbar fixed-bottom navbar-light bg-light">
+        <div className="container">
+          <div className="d-flex justify-content-between align-items-center w-100">
+            <i className="material-icons">menu</i>
+            <Link href="/questions/received">
+              <a>
+                <i className="material-icons">home</i>
+              </a>
+            </Link>
+            <i className="material-icons">person</i>
+          </div>
+        </div>
+      </nav>
       <ToastContainer />
     </div>
   )
